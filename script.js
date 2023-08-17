1bcf32d10f7b102ae2953cf72f2fc0a7d7724f3f
abe <photographyisme7787@gmail.com>
2023-08-17 06:16:16 -0400

    dot count condition for weight height ratio to bound multdriectional

--- script.js
+++ script.js
@@ -32,4 +32,49 @@ class Connect {
     this.connectArea.y = this.centerY
 
   }
+
+  resize() {
+    this.width = window.innerWidth
+    this.height = window.innerHeight
+    this.centerX = this.width / 2 | 0
+    this.centerY = this.height / 2 | 0
+
+    canvas.width = this.width
+    canvas.height = this.height
+
+    this.connectArea.destX = this.centerX
+    this.connectArea.destY = this.centerY
+
+    this.bounds.right = this.width - 2
+    this.bounds.bottom = this.height - 2
+
+    this.colorCounter = 0
+    this.dotCount = this.width * this.height / 3000 | 0
+
+    if (this.dotCount > this.dots.length) {
+      for (let i = this.dotCount - this.dots.length; i > 0; i--) {
+        this.dots.push(
+          new Dot(
+            this.width,
+            this.height,
+            (((this.colorCounter += 2) < 360) ? this.colorCounter : this.colorCounter = 0)
+          )
+        )
+      }
+    } else if (this.dotCount < this.dots.length) {
+
+      this.dots.splice(0, this.dotCount - this.dots.length)
+
+      for (let i = 0; i < this.dotCount; i++) {
+        if (this.dots[i].y < this.bounds.top ||
+          this.dots[i].y > this.bounds.bottom ||
+          this.dots[i].x < this.bounds.left ||
+          this.dots[i].x > this.bounds.right) {
+          this.dots[i].x = Math.random() * this.width | 0
+          this.dots[i].y = Math.random() * this.height | 0
+        }
+      }
+    }
+
+  }
 }
